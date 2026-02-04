530a23cd7199087eca85c3dd31fc15ada2dfec29
sarban <sarban7@mail.ru>
2026-02-04 15:42:06 +0300

    Созданы DTO и Models

--- MessengerAPI/DTOs/LoginRequest.cs
+++ MessengerAPI/DTOs/LoginRequest.cs
@@ -1,6 +1,17 @@
-﻿namespace MessengerAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessengerAPI.DTOs
 {
+    /// <summary>
+    /// DTO для входа
+    /// </summary>
     public class LoginRequest
     {
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        public string Password { get; set; } = null!;
     }
 }
--- MessengerAPI/DTOs/MessageRequest.cs
+++ MessengerAPI/DTOs/MessageRequest.cs
@@ -1,6 +1,16 @@
-﻿namespace MessengerAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessengerAPI.DTOs
 {
+    // DTO для отправки сообщения
     public class MessageRequest
     {
+        // ID получателя
+        [Required(ErrorMessage = "RecipientId обязателен")]
+        public string RecipientId { get; set; } = null!;
+
+        // Текст сообщения
+        [Required(ErrorMessage = "Текст сообщения обязателен")]
+        public string Text { get; set; } = null!;
     }
 }
